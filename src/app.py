2e6b0f031beb0459ebf8ff33f4c40019cfeb0179
HyopangWorld <hyopangworld@gmail.com>
2022-11-07 19:58:16 +0900

    입력기 완성

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 import dash_editor_components
 import dash
 from dash import html
-from dash.dependencies import Input, Output, State
 
-import io
-import sys
+import dash_core_components as dcc
+from dash.dependencies import Input, Output, State
 
 app = dash.Dash(__name__)
 
@@ -12,8 +11,34 @@ app.layout = html.Div([
     dash_editor_components.PythonEditor(
         id='input'
     ),
+    html.Br(),
     html.Button('Submit', id='submit-val', n_clicks=0),
-    html.Pre(id='my-output')
+    html.Br(),
+    html.Pre(id='my-output'),
+    # html.Div(
+    #     children=[
+    #         html.Div(dcc.Graph(animate=True, id='graph-1'), className="six columns"),
+    #         html.Img(
+    #             src="https://t1.daumcdn.net/cfile/tistory/24283C3858F778CA2E",
+    #             style={
+    #                 'position': 'relative',
+    #                 'top': '-400px',
+    #                 'z-index': -1
+    #             }
+    #         )
+    #     ]
+    # ),
+    # html.Img(
+    #     src="https://png.pngtree.com/png-vector/20201112/ourmid/pngtree-cartoon-character-of-a-teen-png-image_2418571.jpg",
+    #     id='player', 
+    #     style={
+    #         'height':'10%',
+    #         'width':'10%',
+    #         'position': 'relative',
+    #         'top': '-400px',
+    #         'z-index': 1
+    #         }
+    # )
 ])
 
 @app.callback(
@@ -21,17 +46,30 @@ app.layout = html.Div([
     Input('submit-val', 'n_clicks'),
     State('input', 'value')
 )
-
 def update_output(n_clicks, value):
-    sys.stdout = buffer = io.StringIO()
-    
-    map = [0, 0, 0, 0]
+    a = [1,1,1,1,1,1,1]
 
-    exec('{}'.format(value))
+    text = '{}'.format(value)
+    exec(text, globals())
+    a = solution(a)
 
-    a = buffer.getvalue()
-
-    return '{} \n\n {}'.format(a, a == "[0,0,0,0]")
+    return 'return : {}'.format(a)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
+def solution(a):
+    alist = []
+
+    for i in range(0, len(a), 1):
+        if a[i] == 1:
+            alist.append(i)
+    
+    return alist
+
+# @app.callback(
+#     Output('player', 'children')
+# )
+# def moveCharactor(alist):
+
+    
